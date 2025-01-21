7db6798a1291a9c468234d1b9088125f341c8913
fzdf0613 <fzdf0613@gmail.com>
2025-01-21 21:58:11 +0900

    feature: Button 컴포넌트 추가
    Button 컴포넌트의 UI 구현 및 Home 에서 확인할 수 있도록 테스트 버튼 배치

---
+++ src/components/common/Button/Button.tsx
@@ -0,0 +1,40 @@
+import styles from "./Button.module.css";
+
+type Style = "primary" | "secondary" | "outline";
+type Size = "sm" | "md" | "lg";
+
+type Props = {
+  children: React.ReactNode;
+  style: Style;
+  size: Size;
+  disabled?: boolean;
+  onClick: () => void;
+};
+
+export default function Button({
+  children,
+  style,
+  size,
+  onClick,
+  disabled = false,
+}: Props) {
+  return (
+    <button
+      className={getClassNames(style, size)}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+}
+
+function getClassNames(style: Style, size: Size) {
+  if (!styles[style]) {
+    throw new Error("유효하지 않은 버튼 스타일입니다.");
+  }
+  if (!styles[size]) {
+    throw new Error("유효하지 않은 버튼 크기입니다.");
+  }
+  return `${styles[style]} ${styles[size]}`;
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,67 @@
+import Button from "../../components/common/Button/Button";
 import styles from "./Home.module.css";
 
 export default function Home() {
   return (
     <main className={styles.container}>
-      <p>TOGE FE Home</p>
+      <p>버튼테스트합시당</p>
+      <div
+        style={{
+          marginTop: "50px",
+          width: "300px",
+          display: "flex",
+          gap: "10px",
+          flexDirection: "column",
+        }}
+      >
+        <Button style="primary" size="sm" onClick={() => console.log("클릭!")}>
+          button
+        </Button>
+        <Button style="primary" size="md" onClick={() => console.log("클릭!")}>
+          button
+        </Button>
+        <Button style="primary" size="lg" onClick={() => console.log("클릭!")}>
+          button
+        </Button>
+        <Button
+          style="primary"
+          size="lg"
+          onClick={() => console.log("클릭!")}
+          disabled
+        >
+          button
+        </Button>
+        <Button
+          style="secondary"
+          size="sm"
+          onClick={() => console.log("클릭!")}
+        >
+          button
+        </Button>
+        <Button
+          style="secondary"
+          size="md"
+          onClick={() => console.log("클릭!")}
+        >
+          button
+        </Button>
+        <Button
+          style="secondary"
+          size="lg"
+          onClick={() => console.log("클릭!")}
+        >
+          button
+        </Button>
+        <Button style="outline" size="sm" onClick={() => console.log("클릭!")}>
+          button
+        </Button>
+        <Button style="outline" size="md" onClick={() => console.log("클릭!")}>
+          button
+        </Button>
+        <Button style="outline" size="lg" onClick={() => console.log("클릭!")}>
+          button
+        </Button>
+      </div>
     </main>
   );
 }
